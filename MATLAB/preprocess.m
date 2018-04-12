79003fb3e958b9eb9b45d2189df23b5df4d547c8
Vyyom-Kelkar <kelkar.20@osu.edu>
2018-04-11 21:16:28 -0400

    Update preprocess.m

--- MATLAB/preprocess.m
+++ MATLAB/preprocess.m
@@ -1,6 +1,13 @@
 import csvimport.*
 
-table = csvimport('train.csv')
-headers = table(1,:)
-tags = table([2:end],1)
-table = table([2:end],[2:end])
+table = csvimport('train.csv');
+headers = table(1,:);
+tags = table(2:end,1);
+table = table(2:end,2:end);
+tags = cell2mat(tags);
+table = cell2mat(table);
+
+test = csvimport('test.csv');
+test_headers = test(1,:);
+test = test(2:end,:);
+test = cell2mat(test);
